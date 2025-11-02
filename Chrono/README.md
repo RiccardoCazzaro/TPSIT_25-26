4c05f524812a0c24458c021fdb9e3c93aa1a5281
Riccardo Cazzaro <ricky.cazzaro@gmail.com>
2025-11-02 10:06:27 +0100

    Completamento esercizio flutter 02: Chrono

--- Chrono/README.md
+++ Chrono/README.md
@@ -1,16 +1,69 @@
-#CHRONO
+# CHRONO
 Cazzaro Riccardo  
 5IE
 
 
 
+
 ## Descrizione
-Il **Cronometro** è un’applicazione interattiva realizzata in Flutter che misura 
-il tempo trascorso mostrando minuti e secondi in modo chiaro e leggibile.
-Alla base del suo funzionamento ci sono due *Stream* collegati tra loro: 
-il primo, chiamato **ticker**, genera a intervalli regolari degli eventi detti *tuck*,
+Il **Cronometro** è un’applicazione interattiva realizzata in Flutter che misura
+il tempo trascorso mostrando minuti e secondi in un display centrale (es. 00:00).  
+Alla base del suo funzionamento ci sono due *Stream* collegati tra loro:
+il primo, chiamato **ticker** genera, a intervalli regolari, degli eventi detti *tick*,
 mentre il secondo *Stream* elabora questi eventi e li trasforma in secondi.  
-L’interfaccia mostra il tempo e presenta due pulsanti.
-Il primo pulsante consente di passare tra gli stati **START**, **STOP** e **RESET**,
-mentre il secondo permette di mettere in **PAUSA** o **RIPRENDERE** (**RESUME**) il cronometro.
+
+
+L'interfaccia, oltre ad essere composta dal display, ha due pulsanti:
+ - lI pulsante principale gestisce gli stati **START**, **STOP** e **RESET**
+   
+ - Il secondo pulsante permette di mettere in **PAUSA** o **RIPRENDERE (RESUME)**
+   il conteggio dal punto in cui si era interrotto
+
+
+<br>
+
+
+
+## Stato
+Il cronometro si attiva quando l'utente preme il pulsante Avvio:  
+
+
+**AVVIO**  
+   - chiama funzione di avvio, la variabile avvio diventa true e pausa diventa false
+   - Questa modifica viene comunicata e tramite setState() l'icona del pulsante si trasforma immediatamente da Play a Stop 
+<br>
+
+
+**STREAM**  
+   - ***Creazione dello stream:*** viene aperto un canale di       comunicazione (StreamController(_controllerTicker))
+    <br>
+   -  ***Avvio:*** viene attivato un timer periodico che scatta ogni 100 millisecondi (0.1 secondi)  
+   - ***Lancio dei Segnali (Tick):*** ad ogni scatto del Timer, un contatore (_tick) aumenta e il suo valore viene inviato nel canale
+   - ***Connessione tramite canale (pipe):*** viene creato il convertitore del tempo (secondiStream()) che riceve tutti i tick.
+    Un Ascoltatore (StreamSubscription(_secondiTot)) viene subito collegato all'uscita del canale    
+
+  <br>
+   
+**ESECUZIONE PIPE** (il convertitore riceve i tick e li conta) <br> 
+- ***Trasformazione:*** La funzione arrivata a 10 tick (10 * 100ms) li converte in un secondo
+ e di conseguenza arrivati a 60 tick li convertirà in un minuto e zero secondi(01:00)  
+- ***Aggiornamento dello Schermo:*** l'Ascoltatore una volta ricevuto il secondo (10 tick) calcola quanti minuti e secondi sono passati
+ e chiama setState() per aggiornare il display visualizzato
+
+<br> 
+
+## Scelte di sviluppo
+- **FloatingActionButton:** utilizza un Widget Row all'interno del floatingActionButton dello Scaffold per posizionare i due pulsanti affiancati in basso a destra
+- **Gestione dello stato con booleani:** variabili avvio e pausa per definire il funzionamento e le icone dei pulsanti
+- **Ascolto tramite .listen():** per rendere esecuzione asincrona senza bloccare l'applicazione
+- **Timer:** per generare il segnale di tempo (tick) ricorrente ogni 100ms che alimenta lo Stream
+- **StreamSubscription:** l'ascoltatore (secondiTot) permette di fermare l'ascolto (.cancel())
+- **StreamController?.close():** per chiudere il canale di input dello Stream (_controllerTicker?.close())
+
+
+
+
+
+
+
 
--- Chrono/lib/main.dart
+++ Chrono/lib/main.dart
@@ -1,70 +1,176 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const ChronoApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Chrono(),
+  ));
 }
 
-class ChronoApp extends StatelessWidget {
-  const ChronoApp({super.key});
+
+class Chrono extends StatefulWidget {
+  const Chrono({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Cronometro Interfaccia',
-      home: ChronoScreen(),
-    );
+  State<Chrono> createState() {
+    return _ChronoState();
   }
 }
 
-class ChronoScreen extends StatefulWidget {
-  const ChronoScreen({super.key});
+class _ChronoState extends State<Chrono> {
+  String displayTempo = "00:00";   
+  bool avvio = false;              
+  bool pausa = false;              
 
-  @override
-  State<ChronoScreen> createState() {
-    return _ChronoScreenState();
+
+  StreamController<int>? _controllerTicker;
+  StreamSubscription<int>? secondiTot;
+  Timer? _timerTicker;
+  int _tick = 0;                   
+
+  Stream<int> secondiStream(Stream<int> tickStream) async* {
+    await for (final int t in tickStream) {
+      if (t % 10 == 0) yield t ~/ 10;
+    }
   }
-}
 
-class _ChronoScreenState extends State<ChronoScreen> {
-  String _displayTime = '00:00.00';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cronometro Interfaccia'),
-        backgroundColor: Colors.red, 
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          _displayTime,
-          style: const TextStyle(
-            fontSize: 80,
-            fontWeight: FontWeight.w800,
-          ),
+  void avvia() {
+    if (avvio) return;
+
+
+    _controllerTicker = StreamController<int>();
+    _tick = 0;
+    _controllerTicker!.add(_tick); 
+
+
+    _timerTicker = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+    _tick++;
+    _controllerTicker!.add(_tick);
+    });
+
+
+    Stream<int> secondi = secondiStream(_controllerTicker!.stream);
+
+
+    secondiTot = secondi.listen((int sec) {
+      if (!pausa) {
+        int min = sec ~/ 60;
+        int s = sec % 60;
+        setState(() {
+        displayTempo =  '${min.toString().padLeft(2, '0')}:${s.toString().padLeft(2, '0')}';
+        });
+      }
+    });
+
+    setState(() {
+      avvio = true;
+      pausa = false;
+    });
+  }
+
+  void reset() {
+    _timerTicker?.cancel();
+    _controllerTicker?.close();
+    secondiTot?.cancel();
+
+    _tick = 0;
+    setState(() {
+      avvio = false;
+      pausa = false;
+      displayTempo = "00:00";
+    });
+  }
+
+  void pausaRiprendi() {
+    if (!avvio) return;
+  
+    if (!pausa) {
+      _timerTicker?.cancel();
+      setState(() {
+        pausa = true; 
+     });
+    } else {
+      _timerTicker = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+        _tick++;
+        _controllerTicker!.add(_tick);
+      });
+      setState(() {
+        pausa = false;
+     });
+    }
+  }
+
+
+@override
+Widget build(BuildContext context) {
+  Icon iconaAvvio;
+  if (avvio) {
+    iconaAvvio = const Icon(Icons.stop);
+  } else {
+    iconaAvvio = const Icon(Icons.play_arrow);
+  }
+
+  Icon iconaPausa;
+  if (pausa) {
+    iconaPausa = const Icon(Icons.play_arrow);
+  } else {
+    iconaPausa = const Icon(Icons.pause);
+  }
+
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text(
+        'CRONOMETRO',  
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
+      backgroundColor: Colors.grey,
+      centerTitle: true,
+    ),
+    
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          FloatingActionButton(
-            heroTag: 'pause_resume',
-            backgroundColor: Colors.red,
-            onPressed: () {},
-            tooltip: 'PAUSE / RESUME',
-            child: const Icon(Icons.pause),
-          ),
-          const SizedBox(width: 29),
-          FloatingActionButton(
-            heroTag: 'control',
-            backgroundColor: Colors.red,
-            onPressed: () {},
-            tooltip: 'START / STOP / RESET',
-            child: const Icon(Icons.play_arrow),
+          Text(
+            displayTempo,
+            style: const TextStyle(fontSize: 90, fontWeight: FontWeight.bold),
           ),
         ],
+      ),
+    ),
+
+  floatingActionButton: Row(
+    mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+           backgroundColor: Colors.grey,
+           tooltip: "PAUSA / RIPRESA",
+           child: iconaPausa,
+           onPressed: () {
+             if (avvio) {
+               pausaRiprendi();
+             }
+           },
+         ), 
+
+    const SizedBox(width: 20),
+
+        FloatingActionButton(
+           backgroundColor: Colors.grey,
+           tooltip: "AVVIO / STOP / RESET",
+           child: iconaAvvio,
+           onPressed: () {
+            if (avvio) {
+               reset();
+             } else {
+               avvia();
+             }
+           },
+          ),
+       ],
       ),
     );
   }
