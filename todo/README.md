a5c002392ca865ad34c6d5bbd1e25acc50282e31
Riccardo Cazzaro <ricky.cazzar@gmail.com>
2026-01-19 13:35:25 +0100

    Aggiornamento  esercizio

--- todo/README.md
+++ todo/README.md
@@ -1,3 +1,17 @@
-# todo
+# Todo List - Stile Google Keep
 
-A new Flutter project.
+Cazzaro Riccardo 5IE
+
+## Descrizione
+Applicazione Todo ispirata a Google Keep.<br>Permette di aggiungere, modificare, completare ed eliminare note Todo  
+
+- Tutti i Todo sono visualizzati come card verticali
+- Tap sul testo per modificarlo direttamente (TextField)
+- Checkbox per segnare come completato
+- FloatingActionButton per aggiungere nuove note
+
+## Scelte di sviluppo
+- **Card + Column invece di ListView.builder:**stile simile a Google Keep.
+- **TextField inline per il testo dei Todo** 
+- **Checkbox per completamento** 
+- **IconButton per eliminare nota**
--- todo/lib/widgets.dart
+++ todo/lib/widgets.dart
@@ -8,8 +8,9 @@ class TodoItem extends StatelessWidget {
   TodoItem({required this.todo}) : super(key: ObjectKey(todo));
 
   final Todo todo;
+  final TextEditingController todoController = TextEditingController();
 
-    TextStyle? _getTextStyle(bool checked) {
+  TextStyle? _getTextStyle(bool checked) {
     if (!checked) return null;
     return const TextStyle(
       color: Colors.black,
@@ -17,7 +18,7 @@ class TodoItem extends StatelessWidget {
     );
   }
 
-@override
+  @override
   Widget build(BuildContext context) {
     final TodoListNotifier notifier = context.read<TodoListNotifier>();
 
@@ -25,24 +26,24 @@ class TodoItem extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         children: [
-          Checkbox( 
+          Checkbox(
             value: todo.checked,
             onChanged: (bool? value) {
               notifier.changeTodo(todo);
             },
           ),
           Expanded(
-            child: TextField( 
-              controller: TextEditingController(text: todo.name),
+            child: TextField(
+              controller: todoController,
               onChanged: (text) => notifier.updateTodo(todo, text),
               decoration: const InputDecoration(
-                border: InputBorder.none, 
+                border: InputBorder.none,
                 hintText: "Nota...",
               ),
               style: _getTextStyle(todo.checked),
             ),
           ),
-          IconButton( 
+          IconButton(
             icon: const Icon(Icons.close, size: 20),
             onPressed: () => notifier.deleteTodo(todo),
           ),
